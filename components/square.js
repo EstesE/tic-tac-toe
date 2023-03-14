d269802025c92ebfc3596c2ea77182755b179981
Mithrilhall <eric_estes@aspensquare.com>
2023-03-14 00:20:27 -0400

    Initial commit

---
+++ components/square.js
@@ -0,0 +1,12 @@
+'use client';
+
+
+const Square = ({ value, onSquareClick }) => {
+  return (
+    <button className="square" onClick={onSquareClick}>{value}</button>
+  );
+};
+
+
+export default Square;
+
